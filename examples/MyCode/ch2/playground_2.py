9810c54306e501ab5bc6db0749d48fb1c780b292
605jimmy <605jimmy@gmail.com>
2021-10-31 18:45:24 +1100

    Finished chapter 2

--- examples/MyCode/ch2/playground_2.py
+++ examples/MyCode/ch2/playground_2.py
@@ -1,23 +1,23 @@
 import math
-# x, y = 7, 3
-# total = x + y
-# print(total)
+x, y = 7, 3
+total = x + y
+print(total)
 
-# print(type(x))
+print(type(x))
 
-# # Calculating the square root of a number.
-# print(9 ** 0.5)
+# Calculating the square root of a number.
+print(9 ** 0.5)
 
-# def stats(num_list):
-#     min_val = min(num_list)
-#     max_val = max(num_list)
-#     range_vals = [min_val, max_val]
+def stats(num_list):
+    min_val = min(num_list)
+    max_val = max(num_list)
+    range_vals = [min_val, max_val]
     
-#     print(f'The min is: {min_val}, max is: {max_val} and range is {range_vals[0]}:{range_vals[1]}')
+    print(f'The min is: {min_val}, max is: {max_val} and range is {range_vals[0]}:{range_vals[1]}')
 
-# stats([47,95,88,73,88,84])
+stats([47,95,88,73,88,84])
 
-
+########### Exercise Code Below ###############
 
 # 2.5 - Circle area, diameter and circumference
 def findings(radius):
@@ -31,14 +31,16 @@ def findings(radius):
 
 findings(2)
 
-#2.6 - Odd or even
+
+# #2.6 - Odd or even
 def even_or_odd(num):
     print('Even' if num % 2 == 0 else 'Odd')
 
 for i in range(5):
     even_or_odd(i)
 
-#2.8 - Table of squares and cubes
+
+# #2.8 - Table of squares and cubes
 def table(num_range):
     # Print the table header
     print('number    Square    Cube')
@@ -49,7 +51,7 @@ def table(num_range):
 table(6)
 
 
-#2.9 - Integer value of a character
+# #2.9 - Integer value of a character
 def char_values(val_list):
     for value in val_list:
         print(ord(value), end=', ')
@@ -57,7 +59,7 @@ def char_values(val_list):
 char_values(['B', 'C', 'D', 'b', 'c', 'd', '0', '1', '2', '$', '*', '+', ' '])
 
 
-#2.10 - Arithmetic, smallest and largest
+# #2.10 - Arithmetic, smallest and largest
 def stats(nums):
     sum_total = sum(nums)
     average = sum_total / len(nums)
@@ -72,7 +74,43 @@ def stats(nums):
 stats([4, 8, 2])
 
 
+#2.11 - Seperating the digits in an integer
 def seperate_digits():
     # Input 5 digits
+    digits = input('Enter a 5 digit number: ')
+    output = ''
     
+    for digit in digits:
+        output += f'   {digit}'
     
+    print(output)
+
+seperate_digits()
+
+
+#2.12 - Investment return
+def investment_return():
+    # Constants
+    RATE = 0.07
+    
+    # Variables
+    principal = int(input('Enter the principal: '))
+    invest_period = int(input('Enter the loan duration (10, 20 or 30): '))
+    a = principal * (1 + RATE) ** invest_period
+    
+    print(f'The return on the your ${principal * invest_period:,.2f} investment is ${a:,.2f}')
+
+investment_return()
+
+
+#2.15 - Sort in ascending order
+def ascending_order(total_nums):
+    num_floats = []
+    
+    for num in range(total_nums):
+        num = int(input('Enter floating point number: '))
+        num_floats.appned(num)
+    
+    return sorted(num_floats)
+
+print(ascending_order(3))
